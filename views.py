60aa5c32e51c8472ed03a5efc70b06b8b8b471d3
SkorpionCode <97434562+SkorpionCode@users.noreply.github.com>
2024-11-26 21:15:15 -0600

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def dashboard_view(request):
-    return render(request, "{% url 'dashboard' %}.html")
-
-def quiz_view(request):
-    return render(request, "{% url 'quiz' %}.html")
-
